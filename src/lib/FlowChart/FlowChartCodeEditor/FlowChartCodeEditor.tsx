a588c8d433db3b4d84edcee9943e6d3253bfe5ac
Phil Scott <philscott.dev@gmail.com>
2020-08-08 22:56:19 -0400

    updating terminal component

--- src/lib/FlowChart/FlowChartCodeEditor/FlowChartCodeEditor.tsx
+++ src/lib/FlowChart/FlowChartCodeEditor/FlowChartCodeEditor.tsx
@@ -3,8 +3,11 @@ import styled from '@emotion/styled'
 import { FC, useRef, useState, useEffect } from 'react'
 import { jsx, css } from '@emotion/react'
 import dynamic from 'next/dynamic'
+import { format } from 'date-fns'
 import { monaco } from '@monaco-editor/react'
 const Editor = dynamic(() => import('@monaco-editor/react'), { ssr: false })
+
+const dateFormat = 'HH:mm:ss'
 
 interface FlowChartCodeEditorProps {
   className?: string
@@ -20,7 +23,19 @@ const FlowChartCodeEditor: FC<FlowChartCodeEditorProps> = ({
     onDidChangeModelContent: (cb: () => void) => void
     getValue: () => void
   }>()
+  const terminalRef = useRef<HTMLDivElement>(null)
   const [isVisible, setVisible] = useState(false)
+  const [terminalValue, setTerminalValue] = useState(
+    `${format(new Date(), dateFormat)}:~$ Initialized \r\n`,
+  )
+
+  useEffect(() => {
+    if (terminalValue && terminalRef.current) {
+      terminalRef.current.scrollTop = terminalRef.current.scrollHeight
+    }
+  }, [terminalValue])
+
+  // useDelayedExpand
   useEffect(() => {
     if (isExpanded) {
       const timeout = setTimeout(() => {
@@ -36,6 +51,25 @@ const FlowChartCodeEditor: FC<FlowChartCodeEditorProps> = ({
     editorRef.current = editor
   }
 
+  const handleEvalScript = () => {
+    const date = format(new Date(), dateFormat)
+    const editor = editorRef?.current ?? null
+    if (editor) {
+      const script = editor.getValue() as string | undefined
+      if (script && script.length) {
+        try {
+          const result = new Function(script)
+          const response = result.call(null, 1)
+          console.log(response)
+          setTerminalValue(terminalValue + `${date}:~$ Success \r\n`)
+        } catch (err) {
+          console.log(err)
+          setTerminalValue(terminalValue + `${date}:~$ Error \r\n`)
+        }
+      }
+    }
+  }
+
   const listenEditorChanges = () => {
     const editor = editorRef?.current ?? null
     if (editor) {
@@ -44,10 +78,24 @@ const FlowChartCodeEditor: FC<FlowChartCodeEditorProps> = ({
       })
     }
   }
+
   return (
-    <div className={className}>
+    <Wrapper className={className} isVisible={isVisible}>
+      <TestButton onMouseDown={handleEvalScript} isVisible={isVisible}>
+        Test Script
+      </TestButton>
       <Editor
         className="editor"
+        wrapperClassName="editor-wrapper"
+        value={
+          '// Use keyword "arguments" to extract values from previous scripts \r\n' +
+          '// Ensure you return a single value to pass to the next script \r\n' +
+          '// Example Script: \r\n' +
+          '\r\n' +
+          'const [arg] = arguments \r\n' +
+          '\r\n' +
+          'return arg + 1'
+        }
         editorDidMount={handleEditorDidMount}
         language="javascript"
         theme="dark"
@@ -58,17 +106,53 @@ const FlowChartCodeEditor: FC<FlowChartCodeEditorProps> = ({
           },
         }}
         css={css`
+          flex: 1;
           display: ${isVisible ? 'visible' : 'none'};
           box-sizing: border-box;
           height: calc((100vh / 2) - 40px - 24px);
           transition: all 0.25s ease-in-out;
         `}
       />
-    </div>
+      <Terminal ref={terminalRef} spellCheck={false} isVisible={isVisible}>
+        {terminalValue}
+      </Terminal>
+    </Wrapper>
   )
 }
 
-export default styled(FlowChartCodeEditor)`
+export default FlowChartCodeEditor
+
+const Wrapper = styled.div<{ isVisible: boolean }>`
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+  display: flex;
+  position: relative;
   background: #202124;
   box-sizing: border-box;
+  transition: all 0.25s ease-in-out;
+`
+
+const TestButton = styled.button<{ isVisible: boolean }>`
+  display: ${({ isVisible }) => (isVisible ? 'visible' : 'none')};
+  position: absolute;
+  right: 16px;
+  top: 16px;
+  z-index: 150;
+  transition: all 0.25s ease-in-out;
+`
+
+const Terminal = styled.div<{ isVisible: boolean }>`
+  display: ${({ isVisible }) => (isVisible ? 'visible' : 'none')};
+  font-family: monospace;
+  flex: 1;
+  border-left: 1px solid #424242;
+  outline: none;
+  border: none;
+  background: #202124;
+  resize: none;
+  padding: 0 16px;
+  max-height: calc((100vh / 2) - 40px - 24px);
+  color: #d4d4d4;
+  overflow-y: auto;
+  white-space: pre-wrap;
+  transition: all 0.25s ease-in-out;
 `
